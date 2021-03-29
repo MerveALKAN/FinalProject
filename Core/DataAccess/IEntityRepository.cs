e0cecf8846fd30ace1660280840dc5f010d54bc1
MerveALKAN <17701037@mersin.edu.tr>
2021-03-29 18:57:55 +0300

    29.03.2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,4 +1,4 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +6,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAcces
 {
     //generic constraint
     //class:referans tip olabilir
@@ -20,6 +20,7 @@ namespace DataAccess.Abstract
         void Update(T entity);
         void Delete(T entity);
 
+        //Core katmanı diğer katmanları referans almaz. Alırsa o katmana bağımlısın demektir. 
       
     }
 }
